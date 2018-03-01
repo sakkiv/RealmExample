7597bdc8affcf012b93eb97cd6c02ca8823151ee
Virtual Stacks <virtualstacks@vs.local>
2018-03-01 09:52:04 +0530

    Initial Commit

---
+++ RealmAppView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmAppView
+//
+//  Created by Virtual Stacks on 01/03/18.
+//  Copyright © 2018 VirtualStacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
